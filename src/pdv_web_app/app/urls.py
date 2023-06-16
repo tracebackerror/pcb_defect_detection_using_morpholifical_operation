121f17d941f1da8026e58195744ef885acaeed7e
ShivamChaurasia01 <118128276+ShivamChaurasia01@users.noreply.github.com>
2023-06-17 00:30:21 +0530

    Code

--- src/pdv_web_app/app/urls.py
+++ src/pdv_web_app/app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.LoginView.as_view(), name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('register', views.register_view, name='register'),
 ]
--- src/pdv_web_app/app/views.py
+++ src/pdv_web_app/app/views.py
@@ -3,9 +3,60 @@ from django.http import HttpResponse
 from django.template import loader
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 from .forms import PCBForm
 from .image_ssim import pcb_defect_detection_algorithm
+
+from django.views.generic.base import TemplateView
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse("index"))
+
+
+def register_view(request):
+    username = request.POST.get('username', None)
+    password = request.POST.get('password', None)
+    user = User.objects.create_user(username, "", password)
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        # A backend authenticated the credentials
+        form = login(request, user)
+        messages.success(request, f' welcome {username} !!')
+        return redirect(reverse("index"))
+    return redirect(reverse("register"))
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        username = self.request.POST.get('username', None)
+        password = self.request.POST.get('password', None)
+
+        user = authenticate(username=username, password=password)
+        #import pdb; pdb.set_trace()
+        if user is not None:
+            # A backend authenticated the credentials
+            form = login(self.request, user)
+            messages.success(self.request, f' welcome {username} !!')
+            return redirect(reverse("index"))
+
+        messages.info(self.request, f'account done not exit plz sign in')
+        return self.render_to_response(context)
+
+@login_required(login_url="/login")
 def index(request):
     template = loader.get_template('index.html')
 
